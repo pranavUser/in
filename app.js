ceaf3f6c727e92069e8f9c25a15b1f09a77f1c64
Pranav <157873954+pranavUser@users.noreply.github.com>
2024-05-03 17:11:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const lenis = new Lenis({
+    duration: 1.5,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+  });
+  
+  function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update();
+    requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf);
+  
